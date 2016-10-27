3a39940ce30a42475c361c528a2b0d6cd33ec285
Kiran Gadhave <kiran.gadhave@outlook.com>
2016-10-27 14:19:54 +0530

    Added ICommand and Bindable Collection

---
+++ WpfMvvmLibrary/BindableCollection.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmLibrary
+{
+    [Serializable]
+    public class BindableCollection<T> : ObservableCollection<T>, ITypedList
+    {
+        string ITypedList.GetListName(PropertyDescriptor[] listAccesories) { return null; }
+
+        PropertyDescriptorCollection ITypedList.GetItemProperties(PropertyDescriptor[] listAccessors)
+        {
+            return TypeDescriptor.GetProperties(typeof(T), PropertyFilter);
+        }
+
+        static readonly Attribute[] PropertyFilter = { BrowsableAttribute.Yes };
+    }
+}
---
+++ WpfMvvmLibrary/Commander.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfMvvmLibrary
+{
+    public class Commander : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// Event Handler to fire canExecute Change event
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Constructor for canExecute check. Will execute if canExecute returns true.
+        /// </summary>
+        /// <param name="execute">Action</param>
+        /// <param name="canExecute">Bool check for execution</param>
+        public Commander(Action<object> execute, Func<object,bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || (_canExecute?.Invoke(parameter) ?? default(bool));
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+    }
+}
